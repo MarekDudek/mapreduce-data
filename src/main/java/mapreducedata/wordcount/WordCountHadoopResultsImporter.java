dbe4d66ce50b863ef6a5d34d06050f146a933847
Marek Dudek <marek.dudek@gmail.com>
2015-03-22 20:39:43 +0100

    Moved word count results importer from Lambda project

---
+++ src/main/java/mapreducedata/wordcount/WordCountHadoopResultsImporter.java
@@ -0,0 +1,31 @@
+package mapreducedata.wordcount;
+
+import com.google.common.collect.Maps;
+import mapreducedata.FileAccessor;
+
+import java.lang.Long;import java.lang.String;import java.util.List;
+import java.util.Map;
+
+public class WordCountHadoopResultsImporter {
+
+    private static final String TABULATOR = "\t";
+
+    public Map<String, Long> importResults(final String file) {
+
+        final List<String> lines = FileAccessor.readLinesFromResources(file);
+
+        final Map<String, Long> results = Maps.newHashMap();
+
+        for (final String line : lines) {
+
+            final String[] fields = line.split(TABULATOR);
+
+            final String word = fields[0];
+            final Long count = Long.parseLong(fields[1]);
+
+            results.put(word, count);
+        }
+
+        return results;
+    }
+}
