21bba57be7b12a3212cb191f88085c4289308bef
Marek Dudek <marek.dudek@gmail.com>
2015-03-23 19:36:20 +0100

    Added Lombok and applied to weather data

--- src/main/java/mapreducedata/maxtemperature/WeatherDataRecord.java
+++ src/main/java/mapreducedata/maxtemperature/WeatherDataRecord.java
@@ -1,43 +1,15 @@
 package mapreducedata.maxtemperature;
 
-import com.google.common.base.MoreObjects;
+import lombok.Data;
 
 import java.util.regex.Pattern;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
+@Data
 public class WeatherDataRecord {
 
     private final String year;
     private final int airTemperature;
     private final String quality;
-
-    public WeatherDataRecord(final String year, final int airTemperature, final String quality) {
-        this.year = checkNotNull(year);
-        this.airTemperature = airTemperature;
-        this.quality = checkNotNull(quality);
-    }
-    
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("year", year)
-                .add("air temperature", airTemperature)
-                .add("quality", quality)
-                .toString();
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public int getAirTemperature() {
-        return airTemperature;
-    }
-
-    public String getQuality() {
-        return quality;
-    }
 
     public boolean isAirTemperaturePresent() {
         return airTemperature != MISSING_TEMPERATURE;
